f43e822acb72ca1452767be8f417ce73e0ac6032
your-user-name-here <kevin.li@usda.gov>
2024-12-27 15:51:48 -0500

    site

---
+++ docs/R/utils-plot.R
@@ -0,0 +1,16 @@
+plot_fun <- function(mod, pred.vars, mod.data, color_by = "margin_dens"){
+  predict.list <- lapply(pred.vars,\(x){ggeffects::ggpredict(mod, x)})
+  names(predict.list) <- pred.vars
+  
+  pred.trends <- dplyr::bind_rows(predict.list, .id = "pred.var") %>%
+    dplyr::rename_with(~as.character(mod$terms[[2]])[length(as.character(mod$terms[[2]]))],
+                       predicted)
+  
+  pred.data <- mod.data %>%
+    select(all_of(c(as.character(mod$terms[[2]])[length(as.character(mod$terms[[2]]))], pred.vars))) %>%
+    mutate(color_by = get(color_by)) %>%
+    pivot_longer(all_of(pred.vars), names_to = "pred.var", values_to = "x") %>%
+    rename(!!color_by := color_by)
+  
+  list(pred.trends, pred.data)
+}
