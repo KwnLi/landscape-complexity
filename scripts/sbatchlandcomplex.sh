e30ddf8284657015f55ed0da624126afd58d55a1
your-user-name-here <kevin.li@usda.gov>
2025-03-08 00:39:13 -0500

    batch

--- scripts/sbatchlandcomplex.sh
+++ scripts/sbatchlandcomplex.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=4
 #SBATCH --get-user-env              ### Import your user environment setup
 #SBATCH --array=1-19                ### Array index
 
@@ -21,4 +21,4 @@ module load miniconda3
 source activate
 conda activate r-invest2
 
-srun Rscript --vanilla /project/geoecoservices/optimization/landscape-complexity/scripts/atlas_max_one.R ${SLURM_ARRAY_TASK_ID}
+srun Rscript --vanilla /project/geoecoservices/optimization/landscape-complexity/scripts/atlas_max_one.R $SLURM_ARRAY_TASK_ID
